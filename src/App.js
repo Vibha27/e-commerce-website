74736994c2b3776d02dfd4ff1fb4a7db76bbac3c
Vibha27 <svibha081@gmail.com>
2025-03-07 22:17:20 +0530

    added folder structure, routings (product details, compare products and 404 Not found) using react-router-dom library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Front end Assignment - KYC Hub</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<h4>Product Details Page</h4>} />
+        <Route path='/product-details' element={<h4>Product Details Page</h4>} />
+        <Route path='/compare-products' element={<h4>Compare Product Page</h4>} />
+        <Route path='*' element={<h4>404 Not found</h4>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Main = () => {
+    // this component is responsible for wrapping header, sidebar and layout
+    return <div>
+      <h2>Front end Assignment - KYC Hub</h2>
+    </div>
+}
